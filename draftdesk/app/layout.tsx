5c86fb27d9083c04484ab2bb7045107ddb51fa04
dane-meister <dane.meister@stonybrook.edu>
2024-07-10 23:02:27 -0400

    Nextjs and tailwind config

---
+++ draftdesk/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "DraftDesk",
+  description: "DraftDesk is a WIP custom text editor for writers, offering chapter-based organization, efficient text file operations, and an integrated spell checker to connect every piece of your narrative.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
